6bb2606342d820619d08ae181faaadb25cec123b
Benzhi Claimant <claimant@localhost>
2000-01-01 00:00:00 +0000

    Client result for 4f683b05-e1ec-4199-a518-c4ffdb104227-bug-6

--- internal/application/review.go
+++ internal/application/review.go
@@ -13,19 +13,6 @@ func (s *Service) Approve(caseID string, cmd ApproveCommand) (*domain.AccessionC
 		return nil, err
 	}
 	now := s.now()
-	prepared, err := s.store.GetCase(caseID)
-	if err != nil {
-		return nil, mapStoreError(err)
-	}
-	if err := prepared.Freeze(cmd.Actor, cmd.Role, now); err != nil {
-		return nil, err
-	}
-	cert := &domain.ReleaseCertificate{ID: s.id("certificate"), CaseID: prepared.ID, ManifestDigest: prepared.FrozenManifest.Digest, CaseRevision: prepared.Revision + 1, ApprovedBy: cmd.Actor, IssuedAt: now.UTC()}
-	serial, err := s.store.AllocateCertificateRecord(cert, prepared.FrozenManifest)
-	if err != nil {
-		return nil, mapStoreError(err)
-	}
-	cert.SerialNumber = serial
 	payload := repository.EventPayloadBuilder(func(_, after *domain.AccessionCase) any {
 		return map[string]any{"approvedBy": cmd.Actor, "serialNumber": after.Certificate.SerialNumber, "manifestDigest": after.Certificate.ManifestDigest}
 	})
@@ -33,6 +20,12 @@ func (s *Service) Approve(caseID string, cmd ApproveCommand) (*domain.AccessionC
 		if err := c.Freeze(cmd.Actor, cmd.Role, now); err != nil {
 			return nil, err
 		}
+		cert := &domain.ReleaseCertificate{ID: s.id("certificate"), CaseID: c.ID, ManifestDigest: c.FrozenManifest.Digest, CaseRevision: c.Revision + 1, ApprovedBy: cmd.Actor, IssuedAt: now.UTC()}
+		serial, err := repository.AllocateCertificate(tx, cert, c.FrozenManifest)
+		if err != nil {
+			return nil, err
+		}
+		cert.SerialNumber = serial
 		if err := repository.AppendAuditEvent(tx, s.id("event"), c.ID, cmd.Actor, "case.frozen", c.Revision+1, now, map[string]any{"manifestDigest": c.FrozenManifest.Digest, "frozenBy": cmd.Actor}); err != nil {
 			return nil, err
 		}
